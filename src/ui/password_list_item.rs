929c7515c49f815bd6bdb7df86ab7e0dd4e4e1d9
Timo k <toger5@hotmail.de>
2021-04-07 01:48:58 +0200

    password list item style and css loading

--- src/ui/password_list_item.rs
+++ src/ui/password_list_item.rs
@@ -41,49 +41,87 @@ mod imp {
             // klass.set_layout_manager_type::<gtk::BinLayout>();
 
             // Make it look like a GTK Entry.
-            klass.set_css_name("entry");
+            klass.set_css_name("box");
+            // klass.add_css
         }
     }
     // impl
     impl ObjectImpl for PasswordListBox {
         fn constructed(&self, obj: &Self::Type) {
             self.parent_constructed(obj);
+            obj.set_css_classes(&["view", "top", "bottom"]);
+            obj.set_halign(gtk::Align::Center);
+            obj.set_size_request(500, -1);
+            obj.set_valign(gtk::Align::Start);
             obj.set_orientation(gtk::Orientation::Vertical);
-            obj.set_spacing(30);
+            obj.set_spacing(10);
+
             let hbox_top = gtk::Box::new(gtk::Orientation::Horizontal, 30);
-            *self.hbox_top.borrow_mut() = Some(hbox_top);
+            obj.append(&hbox_top);
+            
             let hbox_bottom = gtk::Box::new(gtk::Orientation::Horizontal, 30);
-            *self.hbox_bottom.borrow_mut() = Some(hbox_bottom);
-            obj.append(self.hbox_top.borrow().as_ref().unwrap());
-            obj.append(self.hbox_bottom.borrow().as_ref().unwrap());
-            // Create the child label.
+            obj.append(&hbox_bottom);
+
             let site_label = gtk::Label::new(Some("github.com"));
-            self.hbox_top.borrow().as_ref().unwrap().append(&site_label);
-            *self.site_label.borrow_mut() = Some(site_label);
-
-            let copy_button = gtk::Button::with_label("Copy");
-            self.hbox_top.borrow().as_ref().unwrap().append(&copy_button);
-            *self.copy_button.borrow_mut() = Some(copy_button);
-
+            site_label.set_hexpand(true);
+            site_label.add_css_class("site-name-label");
+            hbox_top.append(&site_label);
+            
+            let hbox_bottom_left = gtk::Box::new(gtk::Orientation::Vertical, 0);
+            hbox_bottom.append(&hbox_bottom_left);
+            
             let password_label = gtk::Entry::new();
+            password_label.set_hexpand(true);
             password_label.set_text("Haga0.RenoBetu");
+            password_label.add_css_class("monospace");
             password_label.set_visibility(false);
             password_label.set_has_frame(false);
             password_label.set_can_focus(false);
-            self.hbox_bottom.borrow().as_ref().unwrap().append(&password_label);
-            let password_show_button = gtk::Button::with_label("Hidden");
-            password_show_button.set_has_frame(false);
-            self.hbox_bottom.borrow().as_ref().unwrap().append(&password_show_button);
-            password_show_button.connect_clicked(glib::clone!(@weak password_label, @weak password_show_button => move |_| {
-                let is_visible = password_label.get_visibility();
-                password_label.set_visibility(!is_visible);
-                if is_visible{
-                    password_show_button.set_label("Hidden");
-                }else{
-                    password_show_button.set_label("Shown");
-                }
-            }));
+            hbox_bottom_left.append(&password_label);
 
+            let hbox_bottom_left_bottom = gtk::Box::new(gtk::Orientation::Horizontal, 20);
+            hbox_bottom_left.append(&hbox_bottom_left_bottom);
+
+            let hbox_linked = gtk::Box::new(gtk::Orientation::Horizontal,0);
+            hbox_linked.add_css_class("linked");
+            let password_short_button = gtk::Button::with_label("short");
+            password_short_button.add_css_class("tiny");
+            let password_normal_button = gtk::Button::with_label("normal");
+            password_normal_button.add_css_class("tiny");
+            let password_long_button = gtk::Button::with_label("long");
+            password_long_button.add_css_class("tiny");
+            hbox_linked.append(&password_normal_button);
+            hbox_linked.append(&password_short_button);
+            hbox_linked.append(&password_long_button);
+            hbox_bottom_left_bottom.append(&hbox_linked);
+            let password_show_button = gtk::Button::with_label("Hidden");
+            password_show_button.set_halign(gtk::Align::End);
+            password_show_button.add_css_class("tiny");
+            password_show_button.set_has_frame(false);
+            password_show_button.connect_clicked(
+                glib::clone!(@weak password_label, @weak password_show_button => move |_| {
+                    let is_visible = password_label.get_visibility();
+                    password_label.set_visibility(!is_visible);
+                    if is_visible{
+                        password_show_button.set_label("Hidden");
+                    }else{
+                        password_show_button.set_label("Shown");
+                    }
+                }),
+            );
+            hbox_bottom_left_bottom.append(&password_show_button);
+
+
+            let copy_button = gtk::Button::with_label("Copy");
+            copy_button.set_valign(gtk::Align::End);
+            copy_button.set_size_request(120,-1);
+            copy_button.add_css_class("suggested-action");
+            hbox_bottom.append(&copy_button);
+            
+            *self.hbox_top.borrow_mut() = Some(hbox_top);
+            *self.hbox_bottom.borrow_mut() = Some(hbox_bottom);
+            *self.site_label.borrow_mut() = Some(site_label);
+            *self.copy_button.borrow_mut() = Some(copy_button);
             *self.password_label.borrow_mut() = Some(password_label);
             *self.password_show_button.borrow_mut() = Some(password_show_button);
         }
@@ -111,7 +149,7 @@ impl PasswordListBox {
     pub fn new() -> Self {
         glib::Object::new(&[]).expect("Failed to create PasswordListBox")
     }
-    pub fn set_site_name(&self, name: &str){
+    pub fn set_site_name(&self, name: &str) {
         let self_ = imp::PasswordListBox::from_instance(&self);
         self_.site_label.borrow().as_ref().unwrap().set_text(name);
     }
